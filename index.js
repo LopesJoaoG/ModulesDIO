4d96656419164aeae12255aea880dfd93c2e7047
LopesJoaoG <lopesjoaogui@gmail.com>
2022-07-16 14:41:51 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.js";
+
+console.log(mostraIdade("Carlos", 36));
+console.log(mostraCidade("Carlos","Curitiba"));
+console.log(mostraHobby("Carlos", "Ler"));
